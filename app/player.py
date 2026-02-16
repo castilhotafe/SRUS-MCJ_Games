d543cbc3300aa3c4e35dcdf9aaf8742042080d80
20149083 <201249083@tafe.wa.edu.au>
2026-02-16 15:17:29 +0800

    test(Player): Test class player to check its properties - Step - 1 g)

--- app/player.py
+++ app/player.py
@@ -1,7 +1,16 @@
 class Player:
     def __init__(self, uid: str, name: str):
-        self._uid = uid
-        self._name = name
+        self.__uid = uid
+        self.__name = name
+
+    @property
+    def uid(self):
+        return self.__uid
+
+    @property
+    def name(self):
+        return self.__name
+
 
     def __str__(self):
-        return f'Player id: {self._uid} -  {self._name}'
+        return f'Player id: {self.__uid}\n Name:  {self.__name}'
---
+++ test/test_player.py
@@ -0,0 +1,17 @@
+import unittest
+from player import Player
+
+
+class TestPlayer(unittest.TestCase):
+
+    def setUp(self):
+        self.player_zero = Player("00", "Marcos")
+
+
+    def test_player_properties(self):
+        self.assertEqual(self.player_zero.uid, "00")
+        self.assertEqual(self.player_zero.name, "Marcos")
+
+
+
+
